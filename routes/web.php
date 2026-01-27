66c8979f82f0d91d73d15e343603d5a35e08075c
nereaa-06 <nereaperezsanchez777@gmail.com>
2026-01-27 17:25:52 +0100

    Creacion de modelos y controllers

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('faltas', FaltaController::class);
+
+Route::get('informe/diario', [InformeController::class, 'diario'])->name('informe.diario');
+Route::get('informe/semanal', [InformeController::class, 'semanal'])->name('informe.semanal');
